182ee6674c785d05976946b6bc5dc8d321bd3ab3
JochenWen <76524725+JochenWen@users.noreply.github.com>
2024-03-10 21:32:48 +0100

    Update streamlit_app.py
    changed order

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,8 +84,8 @@ df = full_game_info_extractor(sublist)
 #print(df)
 fig = px.scatter(df, x="opponent_civ", y="duration", color = "result")
 #fig.show()
-st.line_chart(df, x="started_at", y="rating")
 
 st.dataframe(df.style.highlight_max(axis=0))
 st.plotly_chart(fig)
+st.line_chart(df, x="started_at", y="rating")
 
