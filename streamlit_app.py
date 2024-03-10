3032122af25223f77d4cbd74f5c459a5370b1a07
JochenWen <76524725+JochenWen@users.noreply.github.com>
2024-03-10 13:51:49 +0100

    Update streamlit_app.py
    user_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import numpy as np
 import plotly.figure_factory as ff
 
+user_input = st.text_input("label goes here", default_value_goes_here)
+
+
 # Add histogram data
 x1 = np.random.randn(200) - 2
 x2 = np.random.randn(200)
