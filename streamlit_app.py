8a335339be75312ab40386b6c0c907b5a13c612b
JochenWen <76524725+JochenWen@users.noreply.github.com>
2024-03-17 22:03:13 +0100

    Update streamlit_app.py
    updated code to make it more scalable, both with respect to plots that can be chosen, but also the metrics that the user might want to display

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import plotly.express as px
 
 
 player_id = int(st.text_input("Enter your AOE4 world string here, i.e. https://aoe4world.com/players/13766994 - the last string after players", "1270139"))
-st.write("You entered:", player_id)
+#st.write("You entered:", player_id)
 
 
 
@@ -47,8 +47,6 @@ def full_game_info_extractor(data):
     for item in data:
         data_output.append(item)
 
-
-
     unnested_data = []
     for game in data_output:
         for team in game['teams']:
@@ -65,7 +63,26 @@ def full_game_info_extractor(data):
     villain_columns = ["opponent_civ", "game_id"]
     df_villain_info = df_villain[villain_columns]
     df_hero = df_hero.merge(df_villain_info, on='game_id', how='left')
+    df_hero["duration"] = df_hero["duration"] / 60
     return df_hero
+
+def interactive_plot(df):
+    select_graph = ["scatter", "bar"]
+    select_plot = st.selectbox("Select desired graph", select_graph)
+
+    x_axis_val = st.selectbox("Select X-Axis Value", options=df.columns)
+    y_axis_val = st.selectbox("Select Y-Axis Value", options=df.columns)
+    color = st.selectbox("If you want to, select a column to color results", options=df.columns)
+    
+    
+    if select_plot:
+        plot_function = getattr(px, select_plot)
+        
+        fig = plot_function(df, x=x_axis_val, y=y_axis_val, color=color)
+        
+        st.plotly_chart(fig)
+    else:
+        st.write("Please select at least one plot type.")
 
 
 def main ():
@@ -73,9 +90,10 @@ def main ():
     result = get_aoe4_data(player_id)
     sublist = create_sublist(result)
     df = full_game_info_extractor(sublist)
-    fig = px.scatter(df, x="opponent_civ", y="duration", color = "result")
-    st.dataframe(df.style.highlight_max(axis=0))
-    st.plotly_chart(fig)
+    message = f'{player_id}, this resolves to the following playername: {df["name"].values[0]}'
+    st.write("You entered:", message)
+    st.dataframe(df.style.background_gradient(cmap='coolwarm'))
+    interactive_plot(df)
 
 if __name__ == "__main__":
     main()
